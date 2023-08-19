a61c70811c164cd9fd1a090289b453a52952e78a
phdljr <phdljr@naver.com>
2023-08-20 03:24:02 +0900

    feat: add jwt

--- src/main/java/kr/ac/phdljr/springbootjpaquerydsl/service/RegisterServiceImpl.java
+++ src/main/java/kr/ac/phdljr/springbootjpaquerydsl/service/RegisterServiceImpl.java
@@ -7,8 +7,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
-import java.util.Optional;
-
 @Service
 @RequiredArgsConstructor
 public class RegisterServiceImpl implements RegisterService {
